20b9dcb6d05d630900a4122b1d24e1b99f99bcb2
Diego <99410932+DrGomez21@users.noreply.github.com>
2025-04-15 16:20:32 -0300

    Agregar notificaciones y mejorar manejo de errores en la descarga de playlists

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react"
+import toast, { Toaster } from "react-hot-toast"
+const API_URL = import.meta.env.VITE_API_URL
 
 function App() {
 
@@ -6,11 +8,17 @@ function App() {
   const [loading, setLoading] = useState(false)
 
   const handleDownload = async () => {
-    if (url == '') return
+    if (url == '') return toast.error("Escribe una url de playlist 🔗", {
+      position: "top-right"
+    })
     setLoading(true)
 
+    toast("Intentamos descargar la playlist. Aguarda un momento.", {
+      position: "top-right"
+    })
+
     try {
-      const response = await fetch('http://localhost:5000/download', {
+      const response = await fetch(`${API_URL}/download`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
@@ -29,26 +37,32 @@ function App() {
       document.body.appendChild(a)
       a.click()
       a.remove()
+      toast.success("Playlist lista 💓", {
+        position: "top-right"
+      })
     } catch (error) {
-      alert('Ocurrió un error: ' + error.message)
+      console.log(API_URL)
+      console.log(error)
+      toast.error("Ocurrió un error 😢", {
+        position: "top-right"
+      })
     } finally {
       setLoading(false)
+      setUrl('')
     }
   }
 
   return (
-    <div className="px-6 py-8 bg-[#FFF6F6] w-screen h-full">
+    <div className="px-6 py-8 bg-[#FFF6F6] w-screen h-full md:h-screen">
 
       <div className="flex flex-col items-center">
         <div className="absolute top-0 bg-[#FF5F5F]/80 w-1/2 h-12 blur-2xl rounded-full"></div>
-     
-        <h1
-          className="font-poppins font-medium text-3xl sm:text-6xl text-center mt-4"
-        >
+
+        <h1 className="font-poppins font-medium text-3xl sm:text-6xl text-center mt-4">
           Tus playlist de<img src="./logo-youtube.png" className="inline w-11 h-11 sm:w-24 sm:h-24" /><span className="font-normal font-instrument">Youtube</span>
           <p>de vuelta a tu 📱 <span className="font-instrument font-normal">dispositivo</span></p>
         </h1>
-        
+
         <input
           type="text"
           value={url}
@@ -70,26 +84,30 @@ function App() {
 
       </div>
 
-      <div className="flex flex-col gap-4 mt-6">
-        <h2 className="font-poppins font-medium text-xl">Cómo usar la página</h2>
-        <div className="flex items-center gap-2 w-full bg-white p-2 rounded-lg">
-          <img src="./artist-1.png" alt="" className="w-28" />
+
+      <div className="grid grid-cols-1 gap-2 sm:grid-cols-3 mt-6">
+        <div className="flex items-center gap-4 col-span-1 bg-white rounded-md p-4">
+          <div className="absolute bg-red-200 w-8 h-8 blur-lg rounded-full"></div>
+          <span className="text-red-500 text-4xl font-instrument">1</span>
           <p className="font-poppins">Pon tu playlist de youtube en pública 🌐 y copia su enlace 🔗</p>
         </div>
-        <div className="flex items-center gap-2 w-full bg-white p-2 rounded-lg">
-          <img src="./artist-2.png" alt="" className="w-28" />
+        <div className="flex items-center gap-4 col-span-1 bg-white rounded-md p-4">
+          <div className="absolute bg-red-200 w-8 h-8 blur-lg rounded-full"></div>
+          <span className="text-red-500 text-4xl font-instrument">2</span>
           <p className="font-poppins">Pega el enlace en el cuadro de arriba ✍🏼</p>
         </div>
-        <div className="flex items-center gap-2 w-full bg-white p-2 rounded-lg">
-          <img src="./artist-3.png" alt="" className="w-28" />
+        <div className="flex items-center gap-4 col-span-1 bg-white rounded-md p-4">
+          <div className="absolute bg-red-200 w-8 h-8 blur-lg rounded-full"></div>
+          <span className="text-red-500 text-4xl font-instrument">3</span>
           <p className="font-poppins">Descomprime el archivo .zip de tu música 📁</p>
         </div>
       </div>
 
-      <footer className="w-full text-center pt-8">
-        <p className="font-poppins">Desarrollado con 💓 por <span className="font-instrument">@godie.codes</span></p>
+      <footer className="w-full mx-4 pt-8">
+        <p className="font-poppins text-center">Desarrollado con 💓 por <span className="font-instrument">@godie.codes</span></p>
       </footer>
 
+      <Toaster />
     </div>
   )
 }
